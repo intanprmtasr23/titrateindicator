81ce96a42618f1a9b4205d673590f3eb6a96ae19
intanprmtasr23 <intanprmtasr23@gmail.com>
2025-07-16 12:11:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,21 @@
-import streamlit as st
-import numpy as np
+Nama indikator,Warna Asam,Warna Basa,Rentang pH
+asam pikrat, tak berwarna, kunging, 0,1- 0,8
+2,6 dinitro fenol, tak berwarna, kuning, 2,0-4,0
+Metil Jingga,Merah,Kuning,3.1 - 4.4
+hijau brom kresol, kuning, biru, 3,8 - 5,4
+pur pur brom kresol, kuning, ungu, 5,2 - 6,8
+Metil Merah,Merah,Kuning,4.4 - 6.2
+merah fenol, kuning, merah, 6,4 - 8,0
+Bromtimol Biru,Kuning,Biru,6.0 - 7.6
+A-naftolftalein, kuning, biru, 7,0 - 9,0
+pur pur kresol, kuning, biru, 7,4 - 9,6
+timol ftalein, tak berwarna, biru, 9,3 - 10,5
+Fenolftalein,Tidak berwarna,Merah Muda,8.3 - 10.0
+Timol Biru,Merah,Biru,1.2 - 2.8
+Lakmus,Merah,Biru,4.5 - 8.3
+Alizarin Kuning,rose,Oranye,10.1 - 12.0
+1,3,5 trinito benzen, tak berwarna, jingga, 12,0 - 14,0
+erio black tea, merah, biru, 6,3 - 11,5
 
-# --- Konfigurasi Halaman ---
-st.set_page_config(
-    page_title="Simulasi Titrasi Asam Basa",
-    layout="centered"
-)
-# --- Judul Aplikasi ---
-st.title("🧪 Simulasi Titrasi Asam Basa")
-st.markdown("---")
-
-# --- Pengaturan Awal Titrasi ---
-st.sidebar.header("Pengaturan Awal")
-initial_volume_acid = st.sidebar.slider(
-    "Volume Asam Awal (mL)",
-    min_value=10, max_value=10, value=10, step=6
-)
-initial_concentration_acid = st.sidebar.slider(
-    "Konsentrasi Asam Awal (M)",
-    min_value=0.01, max_value=1.0, value=0.1, step=0.01, format="%.2f"
-)
-concentration_base = st.sidebar.slider(
-    "Konsentrasi Basa (M)",
-    min_value=0.01, max_value=1.0, value=0.1, step=0.01, format="%.2f"
-)
 
 
